27e7763c5b6fe9ae12175b4a0a7a75a6d6a82938
Jeremy <jpitzeruse@gmail.com>
2013-01-11 18:11:40 -0500

    Updated gitignore

--- server.js
+++ server.js
@@ -1,53 +1,53 @@
 //setup Dependencies
 var connect = require('connect')
-    , express = require('express')
-    , io = require('socket.io')
-    , port = (process.env.PORT || 8081);
+		, express = require('express')
+		, io = require('socket.io')
+		, port = (process.env.PORT || 8081);
 
 //Setup Express
 var server = express.createServer();
 server.configure(function(){
-    server.set('views', __dirname + '/views');
-    server.set('view options', { layout: false });
-    server.use(connect.bodyParser());
-    server.use(express.cookieParser());
-    server.use(express.session({ secret: "shhhhhhhhh!"}));
-    server.use(connect.static(__dirname + '/static'));
-    server.use(server.router);
+		server.set('views', __dirname + '/views');
+		server.set('view options', { layout: false });
+		server.use(connect.bodyParser());
+		server.use(express.cookieParser());
+		server.use(express.session({ secret: "shhhhhhhhh!"}));
+		server.use(connect.static(__dirname + '/static'));
+		server.use(server.router);
 });
 
 //setup the errors
 server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.jade', { locals: { 
-                  title : '404 - Not Found'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX' 
-                },status: 404 });
-    } else {
-        res.render('500.jade', { locals: { 
-                  title : 'The Server Encountered an Error'
-                 ,description: ''
-                 ,author: ''
-                 ,analyticssiteid: 'XXXXXXX'
-                 ,error: err 
-                },status: 500 });
-    }
+		if (err instanceof NotFound) {
+				res.render('404.jade', { locals: { 
+									title : '404 - Not Found'
+								 ,description: ''
+								 ,author: ''
+								 ,analyticssiteid: 'XXXXXXX' 
+								},status: 404 });
+		} else {
+				res.render('500.jade', { locals: { 
+									title : 'The Server Encountered an Error'
+								 ,description: ''
+								 ,author: ''
+								 ,analyticssiteid: 'XXXXXXX'
+								 ,error: err 
+								},status: 500 });
+		}
 });
-server.listen( port);
+server.listen( port );
 
 //Setup Socket.IO
 var io = io.listen(server);
 io.sockets.on('connection', function(socket){
-  console.log('Client Connected');
-  socket.on('message', function(data){
-    socket.broadcast.emit('server_message',data);
-    socket.emit('server_message',data);
-  });
-  socket.on('disconnect', function(){
-    console.log('Client Disconnected.');
-  });
+	console.log('Client Connected');
+	socket.on('message', function(data){
+		socket.broadcast.emit('server_message',data);
+		socket.emit('server_message',data);
+	});
+	socket.on('disconnect', function(){
+		console.log('Client Disconnected.');
+	});
 });
 
 
@@ -58,31 +58,31 @@ io.sockets.on('connection', function(socket){
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
 server.get('/', function(req,res){
-  res.render('index.jade', {
-    locals : { 
-              title : 'Your Page Title'
-             ,description: 'Your Page Description'
-             ,author: 'Your Name'
-             ,analyticssiteid: 'XXXXXXX' 
-            }
-  });
+	res.render('index.jade', {
+		locals : { 
+							title : 'Your Page Title'
+						 ,description: 'Your Page Description'
+						 ,author: 'Your Name'
+						 ,analyticssiteid: 'XXXXXXX' 
+						}
+	});
 });
 
 
 //A Route for Creating a 500 Error (Useful to keep around)
 server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
+		throw new Error('This is a 500 Error');
 });
 
 //The 404 Route (ALWAYS Keep this as the last route)
 server.get('/*', function(req, res){
-    throw new NotFound;
+		throw new NotFound;
 });
 
 function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
+		this.name = 'NotFound';
+		Error.call(this, msg);
+		Error.captureStackTrace(this, arguments.callee);
 }
 
 
