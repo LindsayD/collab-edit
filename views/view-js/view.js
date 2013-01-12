96484297b7494a5c8625847b9dd7d60abb5b9e27
Brendan Conron <Brendan.Conron@tufts.edu>
2013-01-11 19:06:23 -0800

    Changed Tabular UI
    Made UI more seemless

--- views/view-js/view.js
+++ views/view-js/view.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 $(document).ready(function(){
-	init();
+	//init();
 	$desk_view = $('#desktop_view');
 	$tab_view = $('#tablet_view');
 	$mob_view = $('#mobile_view');
@@ -8,23 +8,38 @@ $(document).ready(function(){
 	$('li').click(function(){
 		var id = this.id;
 		if(id === "desktop_switch"){
-			$tab_view.fadeOut('fast');
-			$mob_view.fadeOut('fast');
-			$desk_view.fadeIn('fast');
+			$tab_view.fadeOut(1, function(){
+				$mob_view.fadeOut(1, function(){
+					$desk_view.fadeIn('fast');
+				});
+			});
+		$('#desktop_switch').addClass('active');
+		$('#tablet_switch').removeClass('active');
+		$('#mobile_switch').removeClass('active');
 		}else if(id === "tablet_switch"){
-			$mob_view.fadeOut('fast');
-			$desk_view.fadeOut('fast');
-			$tab_view.fadeIn('fast');
+			$mob_view.fadeOut(1, function(){
+				$desk_view.fadeOut(1, function(){
+					$tab_view.fadeIn('fast');
+				});
+			});
+			$('#tablet_switch').addClass('active');
+			$('#desktop_switch').removeClass('active');
+			$('#mobile_switch').removeClass('active');
 		}else if(id == "mobile_switch"){
-			$desk_view.fadeOut('fast');
-			$tab_view.fadeOut('fast');	
-			$mob_view.fadeIn('fast');
+			$desk_view.fadeOut(1, function(){
+				$tab_view.fadeOut(1, function(){
+					$mob_view.fadeIn('fast');
+				});	
+			});
+			$('#mobile_switch').addClass('active');
+			$('#desktop_switch').removeClass('active');
+			$('#tablet_switch').removeClass('active');
 		}
 	});
 });
 
 //init function, currently sets page borders
-function init(){
+/*function init(){
 	var h = $('.tabs li').height();
 	$('#tab_container').height(h);	
-}
+}*/
