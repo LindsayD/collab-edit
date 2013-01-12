8c7223a779a665c9b5117ae9d509bbee18b8f42b
Jeremy <jpitzeruse@gmail.com>
2013-01-12 14:58:29 -0500

    Edit Template Update

--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,39 @@
 /* Author: YOUR NAME HERE
 */
 
-function updateEditor() {
+// [ {emailAddress:String, lineNumber:1-based number, charIndex:0-based number}, ... ]
+var userPositions = []; 
 
+function updateEditor(data) {
+// TODO: Implement
 }
 
-function updateViews() {
+function updateViews(data) {
+// TODO: Implement
+}
 
+function updateCursors(data) {
+	var i, j;
+	for (i = 0; i < data.length; i++) {
+		for (j = 0; j < userPositions.length; j++) {
+			if (data[i].emailAddress === userPositions[j].emailAddress) {
+				userPositions[j].lineNumber = data[i].lineNumber;
+				userPositions[j].charIndex = data[i].charIndex;
+				break;
+			}
+		}
+		// not found, add the user position
+		userPositions[userPositions.length] = {
+			emailAddress: data[i].emailAddress,
+			lineNumber: data[i].lineNumber,
+			charIndex: data[i].charIndex
+		};
+	}
 }
 
 function sessionStart(data, docId) {
 	if (typeof(data) !== 'undefined' && data !== null && data.sessionKey !== null) {
+		me = data.emailAddress;
 		$('#username').text(data.emailAddress);
 		$('#gravatar').attr('src', data.gravatar.avatar + '?s=35');
 		getUserName($('#username'), data.gravatar.profile); 
@@ -20,6 +43,8 @@ function sessionStart(data, docId) {
 	}
 }
 var socket;
+var me = null;
+
 function getDocId() {
 	var path = window.location.pathname;
 	return path.substring(path.lastIndexOf('/') + 1);
@@ -67,12 +92,26 @@ $(document).ready(function() {
 	users.append(elUser);
 	getUserName(elUser.children('span'), data.gravatar.profile);
 	
+	var txtUser = elUser.html();
+	if (data.username !== me) {
+		$('#dropusers')
+			.attr('data-content', '<div><img style="float: left; margin: 0 5px 5px 5px;" src="' + data.gravatar.avatar + '?s=50" /> ' + data.username + ' joined.</div>')
+			.attr('title', '')
+			.popover({ html: true, placement: 'left', trigger: 'manual' }).popover('show');
+		
+		setTimeout(function() { $('#dropusers').popover('hide'); }, 3500);
+	}
+	
 	$('#usercount').text(users.children().length);
   });
   
   socket.on('edit', function(data){
 	updateEditor(data);
 	updateViews(data);
+  });
+  
+  socket.on('change_cursor', function(data){
+	updateCursors(data);
   });
   	
    $('#playbackSlider').change(function(){
